429981741212abfa116cd661203ac8f4f01a08bb
Aleksandra Stocka <aleksandra.stocka689@gmail.com>
2018-12-15 19:06:21 +0100

    add BlogDbContext with configuration

---
+++ Blog/Blog/Context/BlogDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Context
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
--- Blog/Blog/Startup.cs
+++ Blog/Blog/Startup.cs
@@ -2,19 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Blog.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Blog
 {
     public class Startup
     {
+        protected IConfigurationRoot Configuration;
+
+        public Startup()
+        {
+            var configurationBuilder = new ConfigurationBuilder();
+            configurationBuilder.AddXmlFile("appsettings.xml");
+            Configuration = configurationBuilder.Build();
+        }
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<BlogDbContext>(
+                builder => builder.UseSqlServer(Configuration["connectionString"]));
             services.AddMvc();
         }
 
@@ -26,6 +41,7 @@ namespace Blog
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseAuthentication();
             app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
         }
